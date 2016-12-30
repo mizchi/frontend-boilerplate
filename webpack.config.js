17c66fb64084d3be8789f8764f44c8695b9b23c8
竹馬光太郎 <mz@mz.local>
2016-12-31 02:27:36 +0900

    Init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const AsyncAwaitPlugin = require('webpack-async-await') ;
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: "public",
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new AsyncAwaitPlugin({})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
