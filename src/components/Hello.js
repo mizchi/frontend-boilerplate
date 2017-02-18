7176dfa0a77cea30a77b65de34b9290bc03c610e
mizchi <miz404@gmail.com>
2017-02-19 05:43:03 +0900

    Fix test

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,6 +4,6 @@ import styles from './styles.css'
 
 export default function Hello () {
   return <div className={styles.Hello}>
-    <div className={styles.title}>Hello2</div>
+    <div className={styles.title}>Hello</div>
   </div>
 }
